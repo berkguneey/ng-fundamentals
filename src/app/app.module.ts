c642bbffc4588b46efce85ddf688ddb75fab3ce0
Berk Can Güney <berkcanguney@gmail.com>
2022-04-23 14:53:47 +0300

    Toastr module added to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 import { EventListComponent } from './events/event-list/event-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail//event-thumbnail.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { EventService } from './shared/services/event.service';
+import { NotificationService } from './shared/services/notification.service';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, ToastrModule.forRoot()],
   declarations: [
     AppComponent,
     EventListComponent,
     EventThumbnailComponent,
     NavbarComponent,
   ],
-  providers: [],
+  providers: [EventService, NotificationService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
